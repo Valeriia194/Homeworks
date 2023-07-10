5054b1ff2d28e0f211d721eee327fd565373db40
iceG <icymail194@gmail.com>
2023-07-10 16:51:54 +0300

    commit

--- main.py
+++ main.py
@@ -1,98 +1,59 @@
 if __name__ == "__main__":
 
-    # # Task 1
+    # # Test 1
+    # numberOne = int(input("Enter first number: "))
+    # numberTwo = int(input("Enter second number: "))
+    # numberThree = int(input("Enter third number: "))
     #
-    # DayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    # flag = True
+    # sum = numberOne + numberTwo + numberThree
+    # multiply = numberOne * numberTwo * numberThree
     #
-    # while flag:
-    #     day = int(input("Enter the number of day (from 1 to 7): "))
-    #     if 0 <= day <= 7:
-    #         print("It's ", DayOfWeek[day - 1])
-    #         flag = False
-    #     else:
-    #         print("Wrong number, try again!")
-
-
-    # # Task 2
+    # ask = input("Sum or Multiply?\n"
+    #             "S or M: ")
     #
-    # months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
-    #           'October', 'November', 'December']
-    # flag = True
-    #
-    # while flag:
-    #     monthNum = int(input("Enter the number of month (from 1 to 12): "))
-    #     if 0 <= monthNum <= 12:
-    #         print("It's ", months[monthNum - 1])
-    #         flag = False
-    #     else:
-    #         print("Wrong number, try again!")
-
-
-    # # Task 3
-    #
-    # num = int(input("Enter the number: "))
-    # if num > 0:
-    #     print("The number is positive!")
-    # elif num < 0:
-    #     print("The number is negative!")
+    # if (ask == "S"):
+    #     print("The sum is: ", sum)
     # else:
-    #     print("The number is equal zero!")
+    #     print("The multiply is ", multiply)
 
-
-    # #Task 4
+    # # Test 2
     #
-    # numOne = int(input("Enter first number: "))
-    # numTwo = int(input("Enter second number: "))
+    # numberOne = int(input("Enter first number: "))
+    # numberTwo = int(input("Enter second number: "))
+    # numberThree = int(input("Enter third number: "))
     #
-    # if numOne == numTwo:
-    #     print("It's equal!")
+    # ask = input("Maximum, minimum or average?\n"
+    #             "Max, Min or Ave: ")
+    #
+    # list = [numberOne, numberTwo, numberThree]
+    # list.sort()
+    #
+    # min = list[0]
+    # max = list[2]
+    # average = round((numberOne + numberTwo + numberThree)/3)
+    #
+    # if (ask=="Max"):
+    #     print(max)
+    # elif (ask=="Min"):
+    #     print(min)
     # else:
-    #     if numOne > numTwo:
-    #         print("Not equal!\n", numOne, numTwo)
-    #     else:
-    #         print("Not equal!\n", numTwo, numOne)
+    #     print(average)
 
+    # Test 3
+    meters = float(input("Enter the meters: "))
+    choose = input("What to convert meters into?\n"
+                   "m - miles\n"
+                   "i - inches\n"
+                   "y - yards\n"
+                   "Your choose: ")
 
-    # Task 5 (Class Task)
+    convertToMiles = 0.00062137*meters
+    convertToInches = 39.370*meters
+    convertToYards = 1.0936*meters
 
-    products = {
-        'apples': 10,
-        'bananas': 15,
-        'oranges': 12,
-        'pears': 8,
-        'kiwi': 20
-    }
-
-    balance = 100
-
-    shopping_cart = []
-
-    while balance > 0:
-
-        for product in products:
-            print(product, products[product])
-
-        choice = input("What do you want to buy? Or type \"q\" if you want to quit\n")
-
-        if choice == "q":
-            break
-
-        if choice in products:
-            if balance >= products[choice]:
-                shopping_cart.append(choice)
-                print(choice, "is added to your shopping cart")
-                balance -= products[choice]
-            else:
-                print("You don't have enough money to buy it :(")
-        else:
-            print("Not find your choice!")
-
-    print("You bought:")
-    shopping_cart_sum = 0
-    for product in shopping_cart:
-        shopping_cart_sum += products[product]
-        print(product)
-
-    print("Your shopping cart sum is:", shopping_cart_sum)
-    print("Your current balance is:", balance)
+    if (choose == "m"):
+        print(meters, "meters = ", convertToMiles, "miles")
+    elif (choose == "i"):
+        print(meters, "meters = ", convertToInches, "inches")
+    else:
+        print(meters, "meters = ", convertToYards, "yards")
