e5939d752829252f137a66e770ab45cf55403d3f
iceG <icymail194@gmail.com>
2023-09-19 19:22:04 +0300

    ready

---
+++ javas.js
@@ -0,0 +1,63 @@
+// TASK 1 ---------------------------------------------------
+
+// let a = prompt("Add ur name:")
+// alert(`Hello ${a}!`)
+
+// TASK 2 ----------------------------------------------------
+
+// const NOW = 2023
+// let a = prompt("Add ur year of birth:")
+// let age = NOW - a
+// alert(`Ur age is ${age} years old`)
+
+// TASK 3 ----------------------------------------------------
+
+// let a = prompt("The length of side:")
+// alert(`Perimeter is ${a*4}!`)
+
+// TASK 4 ----------------------------------------------------
+
+// const PI = 3.14
+// let a = prompt("The radius of circle:")
+// alert(`Square of circle is ${a*a*PI}!`)
+
+// TASK 5 ----------------------------------------------------
+
+// let a = prompt("The distance is(km):")
+// let b = prompt("Time to arrive (in hours):")
+// alert(`Speed u need: ${a/b} km/h!`)
+
+// TASK 6 ----------------------------------------------------
+
+// const EURO = 0.94
+// let a = prompt("Dollars to change:")
+// alert(`U got: ${a*EURO} euros!`)
+
+// TASK 7 ----------------------------------------------------
+
+// let a = prompt("Size of flash(GB):")
+// let howmuch = (a*1024)/820
+// alert(`Can save: ${Math.trunc(howmuch)} files size of 820MB!`)
+
+// TASK 8 ----------------------------------------------------
+
+// let a = prompt("Money in the pocket:")
+// let b = prompt("Choco cost:")
+// let howmuch = a/b
+// let change = a - Math.trunc(howmuch)*b
+// alert(`U got: ${Math.trunc(howmuch)} chocolades and ${change.toFixed(2)} is your change!`)
+
+// TASK 9 ----------------------------------------------------
+
+// let a = prompt("Enter the number:")
+// let one = (a/100)-((a%100)/100)
+// let three = a%10
+// let two = Math.trunc((a/10)%10)
+// let result = (three*100)+(two*10)+one
+// alert(`Palindrom is: ${result}!`)
+
+// TASK 10 ----------------------------------------------------
+
+let a = prompt("Enter the number:")
+let result = (a%2==0)
+alert(`Is it pair: ${result}!`)
