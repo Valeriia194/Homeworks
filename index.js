501f56c98ce8c2815ab032e34271c63c1304be60
iceG <icymail194@gmail.com>
2023-09-25 16:31:10 +0300

    1

---
+++ index.js
@@ -0,0 +1,20 @@
+// TASK 1 -----------------------------------------------
+
+// let a = prompt("How old are you?")
+// if (a>0&&a<=12){
+//     alert("U are children")
+// }
+// else if(a>12&&a<=18){
+//     alert("U are teenager")
+// }
+// else if(a>18&&a<=60){
+//     alert("U are adult")
+// }
+// else if(a>60){
+//     alert("U are pensioner")
+// }
+// else {
+//     alert("No way")
+// }
+
+// TASK 2 -----------------------------------------------
